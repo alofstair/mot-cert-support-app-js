5e3f15df85c830302637ef4bfb3791d8f681339a
agoodall <agoodall@workforcesoftware.com>
2024-04-11 16:13:59 +0100

    Adding tests - they passed

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -16,15 +16,30 @@ describe('My Login application', () => {
 
 describe('My Login application', () => {
 
-    it('should login with valid credentials', async () => {
+   it('should login with valid credentials', async () => {
+      await browser.url(`http://localhost:3000/#/login`)
+
+     await $('input[name="email"]').setValue('admin@test.com')
+       await $('input[name="password"]').setValue('password123')
+      await $('button').click()
+
+    const element = await $('.card-title')
+       await expect(element).toHaveText('Projects')
+  })
+
+})
+
+describe('My log in test', () => {
+
+    it('should not be able to login with invalid credentials', async () => {
         await browser.url(`http://localhost:3000/#/login`)
 
-        await $('input[name="email"]').setValue('admin@test.com')
-        await $('input[name="password"]').setValue('password123')
+        await $('input[name="email"]').setValue('admin3@test.com')
+        await $('input[name="password"]').setValue('password1233')
         await $('button').click()
 
-        const element = await $('.card-title')
-        await expect(element).toHaveText('Projects')
+        const element = await $('.alert.alert-danger.mt-2')
+        await expect(element).toHaveText('Invalid email or password!')
     })
 
 })
